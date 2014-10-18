c1d249d4b040c64923154144f09859f25a995bdb
yihua <yihua.guo.gcg@gmail.com>
2014-10-17 23:11:19 -0400

    strategy

--- src/com/barracuda/contest2014/Strategy.java
+++ src/com/barracuda/contest2014/Strategy.java
@@ -1,14 +1,16 @@
 package com.barracuda.contest2014;
 
 public class Strategy {
-	public double boardEvaluation(int[][][] board, double ourAdv, int tokens, int opToken) {
+	public double boardEvaluation(int[][][] board, int playerID, double ourAdv, int tokens, int opToken) {
 		double eval = ourAdv;
-		int[][][] allTetra = Utils.allTetrahedron(board);
+		int[][][] allTetra = Utils.allTetrahedron(board, playerID);
 		
 		for (int z = 9; z > 0; z--) {
 			for (int x = 9 - z; x >= 0; x--) {
 				for (int y = 9 - z - x; y >= 0; y--) {
-					
+					if (true) {
+						
+					}
 				}
 			}
 		}
